7e586ad121ea914753520e01149a6710dd36108d
William Greig <wpg6zmk@virginia.edu>
2023-03-25 15:12:41 -0400

    initial commit

--- mysite/studysprint/models.py
+++ mysite/studysprint/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    computing_id = models.CharField()
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.computing_id + ": " + self.first_name + " " + self.last_name
+
+class Assignment(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    link = models.CharField()
+    due_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title + ": " + self.due_date
+
+class Course(models.Model):
+    title = models.CharField(max_length=50)
+    department = models.CharField(max_length=30)
+    assignments = models.ManyToManyField(Assignment)
+    students = models.ManyToManyField(Student)
+
+    def __str__(self):
+        return self.department + ": " + self.title
+
+class Instructor(models.Model):
+    computing_id = models.CharField()
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    courses = models.ManyToManyField(Course)
+
+    def __str__(self):
+        return self.computing_id + ": " + self.first_name + " " + self.last_name
